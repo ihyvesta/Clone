0b23ce663629a8a18b0c41d233aab5311b2eddb8
ihyvesta <160210388+ihyvesta@users.noreply.github.com>
2024-06-17 18:22:38 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Clone
 fi
-cd /Clone
+cd /Hi 
 pip3 install -U -r requirements.txt
 echo "Starting Clone...."
 python3 bot.py
